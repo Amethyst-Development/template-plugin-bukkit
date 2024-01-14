2b6f21fe2b66272bbfb916370abd456d91a3399a
greenlantern456 <swingx@zohomail.in>
2024-01-14 12:04:37 +0530

    Template plugin pushed

---
+++ template-plugin/src/main/java/me/greenlantern456/plugin.java
@@ -0,0 +1,10 @@
+package me.greenlantern456;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello world");
+    }
+}
