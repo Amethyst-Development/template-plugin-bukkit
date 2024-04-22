87cc4dbf7c49166703ceffefb07f336db7e86d45
greenlantern456 <swingx@zohomail.in>
2024-04-22 22:57:25 +0530

    Automatically generate plugin.yml

--- template-plugin/src/main/java/org/amethystdev/plugin.java
+++ template-plugin/src/main/java/org/amethystdev/plugin.java
@@ -5,6 +5,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class plugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("Hello world");
+        getLogger().info("Hello, world");
         }
 }
