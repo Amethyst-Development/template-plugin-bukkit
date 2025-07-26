91ce5193a9d55473796964ef7eba610b5429254e
Phruty <150899336+phruty@users.noreply.github.com>
2025-07-26 18:26:07 +0530

    Update

---
+++ template/src/main/java/org/amethystdev/Bootstrapper.java
@@ -0,0 +1,11 @@
+package org.amethystdev;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+public class Bootstrapper implements PluginBootstrap {
+    @Override
+    public void bootstrap(BootstrapContext context) {
+
+    }
+}
